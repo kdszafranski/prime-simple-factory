ef2cfd31f6637965e4ff8329c904c470761eea47
Kris Szafranski <kdszafranski@gmail.com>
2016-08-30 12:03:37 -0500

    rewind to lecture start

--- server/public/scripts/controllers/addressController.js
+++ server/public/scripts/controllers/addressController.js
@@ -1,9 +1,6 @@
-myApp.controller('AddressController', ['$scope', 'exampleFactory', function($scope, exampleFactory) {
+myApp.controller('AddressController', ['$scope', function($scope) {
   console.log('Address Controller');
 
-  $scope.dataFactory = exampleFactory;
   $scope.message = 'Addresses!';
-  $scope.people = $scope.dataFactory.people;
-  $scope.recentName = $scope.dataFactory.thing;
 
 }]);
--- server/public/scripts/controllers/peopleController.js
+++ server/public/scripts/controllers/peopleController.js
@@ -1,25 +1,14 @@
-myApp.controller('PeopleController', ['$scope', 'exampleFactory', function($scope, exampleFactory) {
+myApp.controller('PeopleController', ['$scope', function($scope) {
   console.log('People Controller');
 
   $scope.message = 'People!';
   $scope.formName = '';
-  $scope.dataFactory = exampleFactory;
-  $scope.people = $scope.dataFactory.people;
-  $scope.thing = $scope.dataFactory.thing;
-  $scope.number = $scope.dataFactory.number();
 
-  // Adds a person and then is visible, via the factory, to the Addresses page.
-  // Does not store in the DB
+  $scope.people = [];
+
   $scope.addPerson = function() {
-    // exampleFactory.people.push($scope.formName);
-    $scope.dataFactory.addPerson($scope.formName);
-    $scope.dataFactory.thing.name = $scope.formName;
+    $scope.people.push({name: $scope.formName});
     $scope.formName = '';
   }
-
-  $scope.addOne = function() {
-    $scope.number = $scope.dataFactory.addOne();
-  }
-
 
 }]);
